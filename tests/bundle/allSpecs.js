6e6d9b7ee57edf7e3c1ec87940964bbeea130a92
leofavre <contato@leofavre.com>
2017-04-11 23:19:23 -0300

    changed Firefox version to latest so ES6 tests won't fail

--- tests/bundle/allSpecs.js
+++ tests/bundle/allSpecs.js
@@ -52,29 +52,3 @@ describe("beforeFirst", function() {
 		).toBe("");
 	});
 });
-
-/*jshint esnext: true */
-
-function add(a, b) { return a + b; }
-
-/*jshint esnext: true */
-
-function subtract(a, b) { return a - b; }
-
-/*jshint esnext: true */
-
-describe("A Calculator", function() {
-
-	it("should be able to add two numbers", function() {
-		expect(add(1, 2)).toEqual(3);
-	});
-
-	it("should be able to subtract two numbers", function() {
-		expect(subtract(3, 2)).toEqual(1);
-	});
-
-	it("should be able to subtract two numbers again", function() {
-		expect(subtract(4, 3)).toEqual(1);
-	});
-
-});
