fc8fb256f1b55d52f983e7da296bede6a971b2d4
leofavre <contato@leofavre.com>
2017-04-11 23:42:19 -0300

    README typo

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 # Canivete
 
-**Canivete** (portuguese for army knife) is a set of Javascript multi-purpouse functions; a personal and on-going project where I keep functions that I find myself writing over and over again.
+**Canivete** (portuguese for army knife) is a set of Javascript multi-purpose functions; a personal and on-going project where I keep functions that I find myself writing over and over again.
 
 **Canivete** is not meant to be downloaded as a single file, its methods should be imported as needed and concatenated with a module bundler like [rollup](https://rollupjs.org/).
--- tests/bundle/allSpecs.js
+++ tests/bundle/allSpecs.js
@@ -5,6 +5,106 @@ function _sliceOnOccurrence(slice, position, delimiter, str) {
 
 /**
  * Dadas duas strings – sentença e delimitador –,
+ * retorna uma string com os caracteres **posteriores
+ * à primeira ocorrência** do delimitador na sentença.
+ * Caso o delimitador não seja encontrado, retorna
+ * uma string vazia.
+ * 
+ * @param  {string} str
+ * @param  {string} delimiter
+ * @return {string}
+ * @public
+ *
+ * @example
+ * afterFirst("paralelepípedo", "le");
+ * // => "lepípedo"
+ */
+const afterFirst = (str, delimiter) => _sliceOnOccurrence("after", "first", delimiter, str);
+
+describe("afterFirst", function() {
+	it("“Paralelepípedo” depois do primeiro “a” é “ralelepípedo”.", function() {
+		expect(
+			afterFirst("Paralelepípedo", "a")
+		).toBe("ralelepípedo");
+	});
+
+	it("“Paralelepípedo” depois do primeiro “e” é “lepípedo”.", function() {
+		expect(
+			afterFirst("Paralelepípedo", "e")
+		).toBe("lepípedo");
+	});
+
+	it("“Paralelepípedo” depois do primeiro “le” é “lepípedo”.", function() {
+		expect(
+			afterFirst("Paralelepípedo", "le")
+		).toBe("lepípedo");
+	});
+
+	it("“Paralelepípedo” depois do primeiro “P” é “aralelepípedo”.", function() {
+		expect(
+			afterFirst("Paralelepípedo", "P")
+		).toBe("aralelepípedo");
+	});
+
+	it("“Paralelepípedo” depois do primeiro “x” (não encontrado) é “”.", function() {
+		expect(
+			afterFirst("Paralelepípedo", "x")
+		).toBe("");
+	});
+});
+
+/**
+ * Dadas duas strings – sentença e delimitador –,
+ * retorna uma string com os caracteres **posteriores
+ * à última ocorrência** do delimitador na sentença.
+ * Caso o delimitador não seja encontrado, retorna
+ * uma string vazia.
+ * 
+ * @param  {string} str
+ * @param  {string} delimiter
+ * @return {string}
+ * @public
+ *
+ * @example
+ * afterLast("paralelepípedo", "le");
+ * // => "pípedo"
+ */
+const afterLast = (str, delimiter) => _sliceOnOccurrence("after", "last", delimiter, str);
+
+describe("afterLast", function() {
+	it("“Paralelepípedo” depois do último “a” é “lelepípedo”.", function() {
+		expect(
+			afterLast("Paralelepípedo", "a")
+		).toBe("lelepípedo");
+	});
+
+	it("“Paralelepípedo” depois do último “e” é “do”.", function() {
+		expect(
+			afterLast("Paralelepípedo", "e")
+		).toBe("do");
+	});
+
+	it("“Paralelepípedo” depois do último “le” é “pípedo”.", function() {
+		expect(
+			afterLast("Paralelepípedo", "le")
+		).toBe("pípedo");
+	});
+
+	it("“Paralelepípedo” depois do último “P” é “aralelepípedo”.", function() {
+		expect(
+			afterLast("Paralelepípedo", "P")
+		).toBe("aralelepípedo");
+	});
+
+	it("“Paralelepípedo” depois do último “x” (não encontrado) é “”.", function() {
+		expect(
+			afterLast("Paralelepípedo", "x")
+		).toBe("");
+	});
+});
+
+/**
+ * Dadas duas strings – sentença e delimitador –,
  * retorna uma string com os caracteres **anteriores
  * à primeira ocorrência** do delimitador na sentença.
  * Caso o delimitador não seja encontrado, retorna
@@ -49,6 +149,56 @@ describe("beforeFirst", function() {
 	it("“Paralelepípedo” antes do primeiro “x” (não encontrado) é “”.", function() {
 		expect(
 			beforeFirst("Paralelepípedo", "x")
+		).toBe("");
+	});
+});
+
+/**
+ * Dadas duas strings – sentença e delimitador –,
+ * retorna uma string com os caracteres **anteriores
+ * à última ocorrência** do delimitador na sentença.
+ * Caso o delimitador não seja encontrado, retorna
+ * uma string vazia.
+ * 
+ * @param  {string} str
+ * @param  {string} delimiter
+ * @return {string}
+ * @public
+ *
+ * @example
+ * beforeLast("paralelepípedo", "le");
+ * // => "parale"
+ */
+const beforeLast = (str, delimiter) => _sliceOnOccurrence("before", "last", delimiter, str);
+
+describe("beforeLast", function() {
+	it("“Paralelepípedo” antes do último “a” é “Par”.", function() {
+		expect(
+			beforeLast("Paralelepípedo", "a")
+		).toBe("Par");
+	});
+
+	it("“Paralelepípedo” antes do último “e” é “Paralelepíp”.", function() {
+		expect(
+			beforeLast("Paralelepípedo", "e")
+		).toBe("Paralelepíp");
+	});
+
+	it("“Paralelepípedo” antes do último “le” é “Parale”.", function() {
+		expect(
+			beforeLast("Paralelepípedo", "le")
+		).toBe("Parale");
+	});
+
+	it("“Paralelepípedo” antes do último “P” é “”.", function() {
+		expect(
+			beforeLast("Paralelepípedo", "P")
+		).toBe("");
+	});
+
+	it("“Paralelepípedo” antes do último “x” (não encontrado) é “”.", function() {
+		expect(
+			beforeLast("Paralelepípedo", "x")
 		).toBe("");
 	});
 });
