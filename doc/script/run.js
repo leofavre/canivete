b4447f5efd9de2f498feeeb0e1f0530d3c1b7f22
Leo Favre <leonardo.favre@inbrands.com.br>
2017-04-13 16:40:06 -0300

    organizing jsdoc related files

--- doc/script/run.js
+++ doc/script/run.js
@@ -1,5 +1,12 @@
 const read = require("fs").readFile;
 const exec = require("child_process").exec;
+const groupBy = require("lodash/groupBy");
+
+
+
+
+
+// funções básicas, como criar e remover pastas ou carregar um JSON de um arquivo.
 
 function toPromise(func, target, options, errMsg = "", processOut = arg => arg, processErr = processOut) {
 	return new Promise((resolve, reject) => {
@@ -23,28 +30,66 @@ const createDir = path => () => execAsPromise(`mkdir -p ${path}`);
 
 const removeDir = path => () => execAsPromise(`rm -r ${path}`);
 
-const exportCommentsAsJsonFile = (path, data, template = "./node_modules/jsdoc-json") => () => execAsPromise(`jsdoc ${path} -d ${data} -t ${template}`);
+const inAlphabeticalOrder = (strA, strB) => (strA > strB) ? +1 : (strA < strB) ? -1 : 0;
 
-const readJsonFile = input => () => readJsonAsPromise(input);
 
-const convertJsonToDocs = json => json.docs ? json.docs.filter(doc => doc.name != undefined) : [];
 
-const exportDocsUsingTemplate = (path, template) => docs => docs.forEach(exportDocUsingTemplate(path, template));
 
-const exportDocUsingTemplate = (path, template) => doc => {
-	let content = prepareContent(doc);
-	return execAsPromise(`ejs-cli ${template} > ${path}/${doc.name}.html -O '${content}'`);
+
+// funções de exportação 
+
+const jsdocAsJson = (path, data, template = "./node_modules/jsdoc-json") => () => execAsPromise(`jsdoc ${path} -d ${data} -t ${template}`);
+
+const readJsonFile = data => () => readJsonAsPromise(data);
+
+const parseJsonFile = json => json.docs ? json.docs.filter(doc => doc.name != undefined) : [];
+
+const exportDocsUsingTemplate = (path, template) => docs => {
+	let menu = prepareMenu(docs);
+	return docs.forEach(exportDocUsingTemplate(path, template, menu));
 };
 
-const prepareContent = (doc, menu) => JSON.stringify(doc);
+const exportDocUsingTemplate = (path, template, menu) => doc => {
+	let content = prepareContent(doc, menu);
+	let name = getDocName(doc);
+	return execAsPromise(`ejs-cli ${template} > ${path}/${name}.html -O '${content}'`);
+};
+
+const prepareMenu = docs => {
+	let groupedMenu = groupBy(docs, byCategoryName);
+	let categoryNames = Object.keys(groupedMenu);
+
+	return categoryNames
+		.sort(inAlphabeticalOrder)
+		.map(prepareSubmenu(groupedMenu));
+};
+
+const prepareSubmenu = (groupedMenu) => categoryName => {
+	let categoryDoc = groupedMenu[categoryName];
+	return {
+		name: categoryName,
+		items: getCategoryFunctions(categoryDoc)
+	};
+};
+
+const prepareContent = (doc, menu) => {
+	doc.menu = menu;
+	return JSON.stringify(doc);
+};
+
+const byCategoryName = doc => doc.tags.filter(tag => tag.title === "category")[0].value;
+
+const getDocName = doc => doc.name;
+
+const getCategoryFunctions = categoryDoc => categoryDoc.map(getDocName);
 
 Promise.resolve()
 	.then(createDir("./doc/temp")) // *
-	.then(exportCommentsAsJsonFile("./dist", "./doc/temp/data.json"))
+	.then(jsdocAsJson("./dist", "./doc/temp/data.json"))
 	.then(readJsonFile("./doc/temp/data.json")) // *
-	.then(convertJsonToDocs)
-	.then(exportDocsUsingTemplate("./doc", "./doc/templates/post.ejs"))
-	.then(removeDir("./doc/temp")) // *
+	.then(parseJsonFile)
+	.then(exportDocsUsingTemplate("./doc", "./doc/templates/index.ejs"))
+	// .then(removeDir("./doc/temp")) // *
 	.catch();
 
 // * etapas desnecessárias se eu conseguisse exportar o conteúdo do JSON na Promise, e não num arquivo
