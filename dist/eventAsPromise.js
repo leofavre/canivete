23a4052bc0b10c9205113ae9c1fa4f2a2a4cc861
Leo Favre <leonardo.favre@inbrands.com.br>
2017-04-17 10:56:55 -0300

    documente eventAsPromise

--- dist/eventAsPromise.js
+++ dist/eventAsPromise.js
@@ -6,26 +6,38 @@
  * and a function that verifies if the event has already
  * happened, which, in turn, receives the DOM Element
  * as parameter.
- * 
+ *
+ * @category Promise
  * @param  {HTMLElement} domElement The DOM Element.
  * @param  {String} eventName The name of the event that will be listened for.
- * @param  {Function} hasAlreadyHappened The verification function.
+ * @param  {Function} [hasAlreadyHappened = domElement => false] The verification function.
  * @return {Promise} When fullfiled, returns the DOM Element.
+ *
+ * @example
+ * var checkbox = document.createElement("input");
+ * checkbox.type = "checkbox";
+ * document.body.appendChild(checkbox);
+ *
+ * eventAsPromise(checkbox, "change")
+ * 	.then(domElement => console.log(domElement.checked));
+ *
+ * // => true
+ * // shown as soon as the checkbox is clicked for the first time
  */
-function eventAsPromise(domElement, eventName, hasAlreadyHappened = domElement => false) {
+const eventAsPromise = (domElement, eventName, hasAlreadyHappened = domElement => false) => {
 	return new Promise(resolve => {
+		const dealWithEvent = evt => {
+			domElement.removeEventListener(eventName, dealWithEvent, true);
+			resolve(domElement);
+		};
+
 		if (hasAlreadyHappened(domElement)) {
 			resolve(domElement);
 		}
 		else {
 			domElement.addEventListener(eventName, dealWithEvent, true);
 		}
-
-		function dealWithEvent(evt) {
-			domElement.removeEventListener(eventName, dealWithEvent, true);
-			resolve(domElement);
-		}
 	});
-}
+};
 
 export default eventAsPromise;
