257f65d130bc3d171691b624da1e1bf07cf78163
leofavre <contato@leofavre.com>
2017-04-19 21:52:09 -0300

    created test file

---
+++ test/clippingInfoSpecs.js
@@ -0,0 +1,9 @@
+import clippingInfo from "../dist/clippingInfo";
+
+describe("clippingInfo", function() {
+	// it(`Should find "rallelepiped" after the first "a" in "Parallelepiped".`, function() {
+	// 	expect(
+	// 		clippingInfo("Parallelepiped", "a")
+	// 	).toBe("rallelepiped");
+	// });
+});
