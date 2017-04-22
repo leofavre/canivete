a3fa52941a4e5947843b30e420219d4b95ca1591
leofavre <contato@leofavre.com>
2017-04-22 09:19:30 -0300

    added tests to removeClass

---
+++ test/removeClassSpecs.js
@@ -0,0 +1,37 @@
+import removeClass from "../dist/removeClass";
+
+describe("removeClass", function() {
+	for (let i = 0; i < 5; i++) {
+		let newEl = document.createElement("div");
+		newEl.className = "oneMore removeClass removeClass-" + i;
+		document.body.appendChild(newEl);
+	}
+
+	it("Should add a CSS class to a single HTMLElement.", function() {
+		let elementHTMLElement = document.querySelector(".removeClass-1");
+		removeClass(elementHTMLElement, "oneMore");
+
+		expect(elementHTMLElement.className).toBe("removeClass removeClass-1");
+	});
+
+	it("Should not change a CSS class when trying to remove an inexistent class.", function() {
+		let elementHTMLElement = document.querySelector(".removeClass-1");
+		removeClass(elementHTMLElement, "oneMore");
+
+		expect(elementHTMLElement.className).toBe("removeClass removeClass-1");
+	});
+
+	it("Should add a CSS class to many HTMLElements.", function() {
+		let elementsNodeList = document.querySelectorAll(".removeClass");
+
+		for (let i = 0, len = elementsNodeList.length; i < len; i++) {
+			elementsNodeList[i].className = "oneMore removeClass";
+		}
+
+		removeClass(elementsNodeList, "oneMore");
+
+		for (let i = 0, len = elementsNodeList.length; i < len; i++) {
+			expect(elementsNodeList[i].className).toBe("removeClass");
+		}
+	});
+});
