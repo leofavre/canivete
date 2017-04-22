bf6fac4cc4af812df8e697dc01bcda57b717ff53
leofavre <contato@leofavre.com>
2017-04-22 09:06:31 -0300

    added removeClass

---
+++ dist/removeClass.js
@@ -0,0 +1,29 @@
+import _changeClassWithMethod from "./internal/dom/_changeClassWithMethod";
+import _domElementsAsArray from "./internal/dom/_domElementsAsArray";
+
+/**
+ * Removes a CSS class from one or many DOM elements.
+ *
+ * @category DOM
+ * @param  {(HTMLElement|HTMLCollection|NodeList|Array.<HTMLElement>|Set.<HTMLElement>)} domEls One or many DOM elements.
+ * @param  {string} str The CSS class.
+ *
+ * @example
+ * let oneElement = document.querySelector(".link.base");
+ * removeClass(oneElement, "link");
+ * 
+ * console.log(oneElement.className);
+ * // => "base"
+ * 
+ * @example
+ * let manyElements = document.querySelectorAll(".link.base");
+ * removeClass(manyElements, "link");
+ *
+ * console.log(manyElements[0].className);
+ * // => "base"
+ */
+const removeClass = (domEls, str) => {
+	_domElementsAsArray(domEls).forEach(domEl => _changeClassWithMethod(domEl, str, "remove"));
+};
+
+export default removeClass;
