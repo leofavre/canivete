9c4ae2760fdb971c22bd386c9d0301c9f985d154
leofavre <contato@leofavre.com>
2017-04-23 14:25:59 -0300

    tested byAlphabeticalOrder successfully

---
+++ test/byAlphabeticalOrderSpecs.js
@@ -0,0 +1,9 @@
+import byAlphabeticalOrder from "../dist/byAlphabeticalOrder";
+
+describe("byAlphabeticalOrder", function() {
+	it("Should order an array by alphabetical order", function() {
+		expect(
+			["Athos", "Porthos", "Aramis"].sort(byAlphabeticalOrder())
+		).toEqual(["Aramis", "Athos", "Porthos"]);
+	});
+});
