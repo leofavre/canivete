3a9ec4d49179821c22260355d72ed3d80e8c92e3
leofavre <contato@leofavre.com>
2017-05-04 23:36:40 -0300

    more tests for ignore

--- test/ignoreSpecs.js
+++ test/ignoreSpecs.js
@@ -61,4 +61,50 @@ describe("listen", function() {
 			done();
 		}, 20);
 	});
+
+	it("Should be able to ignore some of all the attached events.", function(done) {
+		let eventCallback = jasmine.createSpy("eventCallback"),
+			multipleDivs = [document.createElement("div"), document.createElement("div")];
+
+		document.body.appendChild(multipleDivs[0]);
+		document.body.appendChild(multipleDivs[1]);
+
+		listen(multipleDivs, "one two three", eventCallback);
+		ignore(multipleDivs, "one two", eventCallback);
+
+		trigger(multipleDivs[0], "one", false);
+		trigger(multipleDivs[0], "two", false);
+		trigger(multipleDivs[0], "three", false);
+		trigger(multipleDivs[1], "one", false);
+		trigger(multipleDivs[1], "two", false);
+		trigger(multipleDivs[1], "three", false);
+
+		setTimeout(() => {
+			expect(eventCallback.calls.count()).toBe(2);
+			done();
+		}, 20);
+	});
+
+	it("Should be able to ignore some of all the attached events.", function(done) {
+		let eventCallback = jasmine.createSpy("eventCallback"),
+			multipleDivs = [document.createElement("div"), document.createElement("div")];
+
+		document.body.appendChild(multipleDivs[0]);
+		document.body.appendChild(multipleDivs[1]);
+
+		listen(multipleDivs, "one two three", eventCallback);
+		ignore(multipleDivs[0], "one two three", eventCallback);
+
+		trigger(multipleDivs[0], "one", false);
+		trigger(multipleDivs[0], "two", false);
+		trigger(multipleDivs[0], "three", false);
+		trigger(multipleDivs[1], "one", false);
+		trigger(multipleDivs[1], "two", false);
+		trigger(multipleDivs[1], "three", false);
+
+		setTimeout(() => {
+			expect(eventCallback.calls.count()).toBe(3);
+			done();
+		}, 20);
+	});
 });
