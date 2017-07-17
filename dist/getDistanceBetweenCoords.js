977e23de75190d0e379dd8d826f5b6532a6cf026
Leo Favre <leonardo.favre@inbrands.com.br>
2017-07-17 10:46:29 -0300

    added getDistanceBetweenCoords

---
+++ dist/getDistanceBetweenCoords.js
@@ -0,0 +1,9 @@
+import toSum from "./toSum";
+
+const getDistanceBetweenCoords = (coordA, coordB) => {
+	return Math.sqrt(coordA
+		.map((coord, index) => Math.pow(coord - coordB[index], 2))
+		.reduce(toSum));
+};
+
+export default getDistanceBetweenCoords;
