f58bb24c19d23d03d5a249aa006d3f0cbbd35aaa
Leo Favre <leonardo.favre@inbrands.com.br>
2017-07-17 12:26:08 -0300

    added more tests

--- dist/getDistanceBetweenCoords.js
+++ dist/getDistanceBetweenCoords.js
@@ -1,5 +1,35 @@
 import toSum from "./toSum";
 
+/**
+ * Calculates and returns the distance between two points,
+ * given their cartesian coordinates, represented, each one,
+ * by an array of numbers.
+ *
+ * For example, the point in a plane A(x, y) should be passed
+ * to the function as the array [x, y]. Likewise, the point in
+ * 3D space A(x, y, z) should be passed as [x, y, z].
+ *
+ * The function can deal with cartesian coordinates in
+ * [n-dimensional spaces](https://en.wikipedia.org/wiki/Euclidean_distance#n_dimensions).
+ *
+ * @category Geometry
+ * @param {Array.<number>} coordA A cartesian coordinate represented as an array.
+ * @param {Array.<number>} coordB A cartesian coordinate represented as an array.
+ * @return {number} The distance between the two cartesian coordinates.
+ *
+ * @example
+ * getDistanceBetweenCoords([0, 0], [3, 4]);
+ * // => 5
+ *
+ * getDistanceBetweenCoords([2, 1], [5, 5]);
+ * // => 5
+ *
+ * getDistanceBetweenCoords([2, 1, 8], [5, 5, 0]);
+ * // => 9.433981132056603
+ *
+ * getDistanceBetweenCoords([2], [5]);
+ * // => 3
+ */
 const getDistanceBetweenCoords = (coordA, coordB) => {
 	return Math.sqrt(coordA
 		.map((coord, index) => Math.pow(coord - coordB[index], 2))
--- test/getDistanceBetweenCoordsSpecs.js
+++ test/getDistanceBetweenCoordsSpecs.js
@@ -1,19 +1,35 @@
 import getDistanceBetweenCoords from "../dist/getDistanceBetweenCoords";
 
 describe("getDistanceBetweenCoords", function() {
-	it("Should calculate de distance betwwen two points on a surface.", function() {
+	it("Should calculate de distance between two points on a surface.", function() {
 		expect(getDistanceBetweenCoords([2, 1], [5, 5])).toBe(5);
 	});
 
-	it("Should calculate de distance betwwen two points on a surface.", function() {
+	it("Should calculate de distance between two points on a surface.", function() {
 		expect(getDistanceBetweenCoords([0, 0], [3, 4])).toBe(5);
 	});
 
-	it("Should calculate de distance betwwen two points on a 3D space.", function() {
+	it("Should calculate de distance between two points on a 3D space.", function() {
 		expect(getDistanceBetweenCoords([2, 1, 8], [5, 5, 8])).toBe(5);
 	});
 
-	it("Should calculate de distance betwwen two points on a 3D space.", function() {
+	it("Should calculate de distance between two points on a 3D space.", function() {
 		expect(getDistanceBetweenCoords([2, 1, 8], [5, 5, 0])).toBe(Math.sqrt(89));
+	});
+
+	it("Should calculate de distance between two points on a 3D space.", function() {
+		expect(getDistanceBetweenCoords([2, 1, 8, 0], [5, 5, 8, 2])).toBe(Math.sqrt(29));
+	});
+
+	it("Should calculate de distance between two points in a line.", function() {
+		expect(getDistanceBetweenCoords([4], [8])).toBe(4);
+	});
+
+	it("Should calculate de distance between two points in a line.", function() {
+		expect(getDistanceBetweenCoords([2], [5])).toBe(3);
+	});
+
+	it("Should calculate de distance between two points in a line.", function() {
+		expect(getDistanceBetweenCoords([-2], [-5])).toBe(3);
 	});
 });
