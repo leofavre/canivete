1c7cc5c65d25e374db07c4853e716e297abff1b2
leofavre <contato@leofavre.com>
2018-09-07 13:19:15 -0300

    Use old Karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,74 +1,23 @@
-const path = require('path');
-
-process.env.CHROME_BIN = require('puppeteer').executablePath();
-
-module.exports = (config) => {
-  config.set({
-    browsers: [
-      'ChromeHeadlessNoSandbox',
-      'FirefoxHeadless'
-    ],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      },
-      FirefoxHeadless: {
-        base: 'Firefox',
-        flags: ['-headless']
-      }
-    },
-    browserNoActivityTimeout: 60000,
-    singleRun: true,
-    frameworks: ['mocha', 'chai-sinon'],
-    proxies: {
-      '/': '/base/packages/'
-    },
-    files: [
-      {
-        pattern: './karma/*.js',
-        watched: false,
-        served: true
-      },
-      {
-        pattern: 'packages/*/src/**/*.test.js',
-        watched: false
-      },
-      {
-        pattern: 'packages/*/src/**/*.css',
-        watched: true,
-        served: true
-      }
-    ],
-    preprocessors: {
-      'packages/*/src/**/*.test.js': ['webpack']
-    },
-    webpack: {
-      mode: 'development',
-      module: {
-        rules: [
-          {
-            test: /\.js$/,
-            use: {
-              loader: 'istanbul-instrumenter-loader',
-              options: {
-                esModules: true
-              }
-            },
-            include: path.resolve('./packages/'),
-            exclude: /((node_modules|dev|dist)(\\|\/|$)|test.js$)/
-          }
-        ]
-      }
-    },
-    mochaReporter: {
-      showDiff: true
-    },
-    reporters: ['mocha', 'coverage-istanbul'],
-    coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true,
-      skipFilesWithNoCoverage: true
-    }
-  });
+module.exports = function(config) {
+	config.set({
+		basePath: "",
+		frameworks: ["jasmine"],
+		files: ["./test/temp/allSpecs.js"],
+		exclude: [],
+		preprocessors: {},
+		reporters: ["progress"],
+		port: 9876,
+		colors: true,
+		logLevel: config.LOG_INFO,
+		autoWatch: false,
+		browsers: ["Firefox", "Chrome"],
+		singleRun: true,
+		concurrency: Infinity,
+		customLaunchers: {
+			Chrome_travis_ci: {
+				base: 'Chrome',
+				flags: ['--no-sandbox']
+			}
+		}
+	});
 };
